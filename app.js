c789d1f9cb2d916882f7429a916b6aee893ede35
Harshal Sanghvi <harshal2030@outlook.com>
2023-05-16 14:37:08 +0530

    added code

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
+const url = (city) => `https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`;
+
 
 async function getWeatherByLocation(city){
-  // use fetch to get data from the api
+  const resp = await fetch(url(city), {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '4142a0c53dmsh4a54d962d95520dp128a72jsn135ac720bba8',
+		  'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+    },
+  });
+  const respData = await resp.json();
 
   addWeatherToPage(respData);
 }
@@ -16,16 +25,31 @@ function generateImageUrl(iconUrl) {
 };
 
 function addWeatherToPage(data){
-  // weather.innerHTML = `
-  // <h2><img src="${generateImageUrl(data.current.condition.icon)}" /> ${temp}°C <img src="${generateImageUrl(data.current.condition.icon)}" /></h2>
-  // <small>${data.current.condition.text}</small>
-  // `;
+  const temp = data.current.feelslike_c;
+
+  const weather = document.createElement('div')
+  weather.classList.add('weather');
+
+  weather.innerHTML = `
+  <h2><img src="${generateImageUrl(data.current.condition.icon)}" /> ${temp}°C <img src="${generateImageUrl(data.current.condition.icon)}" /></h2>
+  <small>${data.current.condition.text}</small>
+  `;
 
 
- //   cleanup
+ //   cleanup 
+  main.innerHTML= "";
+  main.appendChild(weather);
 };
 
 
+
 form.addEventListener('submit',(e) =>{
-  
+  e.preventDefault();
+
+  const city = search.value;
+
+  if(city){
+    getWeatherByLocation(city)
+  }
+
 });
